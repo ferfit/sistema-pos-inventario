e8cac3c5d9cdc034c194e1bd27e5acf019956c99
Fernando Pereyra <fpereyra@inxap.com>
2021-12-21 14:47:40 -0300

    agrega vista cateogira

--- controladores/categoriasControlador.php
+++ controladores/categoriasControlador.php
@@ -5,5 +5,18 @@ class CategoriasControlador{
     public function ctrPlantilla(){
         include "vistas/plantilla.php";
     }
+
+
+    /*----------------------------------------- */
+    /*         Crear Categoria                  */
+    /*----------------------------------------- */
+    static public function ctrCrearCategoria(){
+
+        if(isset($_POST['nombre'])){
+            
+        }
+
+    }
+
 }
 
